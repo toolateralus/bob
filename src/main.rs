96dd8dc5809f4bee7906cb6599f0d51516686e35
toolateralus <joshnjoshgaming@gmail.com>
2024-05-30 16:11:29 -0400

    Refactor create_makefile function to generate_makefile

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use std::fs::File;
-use std::io::Read;
-
-fn err(value: Result<usize, std::io::Error>) {
+fn handle_io_err(value: Result<usize, std::io::Error>) {
     match value {
         Ok(_) => {}
         Err(err) => panic!("{:?}", err),
@@ -11,7 +8,7 @@ fn err(value: Result<usize, std::io::Error>) {
 fn read_option(stdin: &std::io::Stdin, prompt: &str) -> String {
     println!("{}", prompt);
     let mut buf = String::new();
-    err(stdin.read_line(&mut buf));
+    handle_io_err(stdin.read_line(&mut buf));
     buf = buf.trim_end_matches("\n").to_string();
     return buf;
 }
@@ -21,7 +18,7 @@ fn read_several_options(stdin: &std::io::Stdin, init_prompt: &str) -> Vec<String
     let mut libs: Vec<String> = Vec::new();
     loop {
         let mut buf = String::new();
-        err(stdin.read_line(&mut buf));
+        handle_io_err(stdin.read_line(&mut buf));
         buf = buf.trim_end_matches("\n").to_string();
 
         if buf.is_empty() {
@@ -37,7 +34,8 @@ fn read_several_options(stdin: &std::io::Stdin, init_prompt: &str) -> Vec<String
     }
     return libs;
 }
-fn create_makefile(
+
+fn generate_makefile(
     proj_name: String,
     use_src_dir: bool,
     use_inc_dir: bool,
@@ -54,51 +52,114 @@ fn create_makefile(
     let src_dir = if use_src_dir { "src/" } else { "" };
 
     let make_vars = format!(
-        "CC := clang\nCFLAGS := -std=c2x {}\nLD_FLAGS := {}\nOBJ_DIR := objs\nBIN_DIR := bin",
+        "CC := clang\n\
+        CFLAGS := -std=c2x {}\n\
+        LD_FLAGS := {}\n\
+        OBJ_DIR := objs\n\
+        BIN_DIR := bin",
         include, libs_formatted
     );
 
-    let wildcard = format!("$(wildcard {}*.c)", src_dir);
-    let objs = if use_src_dir {
+    let srcs_wildcard = format!("$(wildcard {}*.c)", src_dir);
+
+    let objs = generate_objs(use_src_dir, &srcs_wildcard);
+
+    let make_file = generate_make_file(use_src_dir, srcs_wildcard, objs, proj_name);
+
+    return make_vars + "\n\n" + &make_file;
+}
+
+fn generate_objs(use_src_dir: bool, wildcard: &String) -> String {
+    if use_src_dir {
         format!("$(patsubst src/%.c,$(OBJ_DIR)/%.o,{})", wildcard)
     } else {
         format!("$(patsubst %.c,$(OBJ_DIR)/%.o,{})", wildcard)
-    };
+    }
+}
 
-   let make_target_all = if use_src_dir {
-    format!("SRCS := {}\nOBJS := {}\n\nall: directories {}\n\ndirectories:\n\tmkdir -p $(OBJ_DIR) $(BIN_DIR)\n\n{}: $(OBJS)\n\t$(CC) $(CFLAGS) -o $(BIN_DIR)/$@ $^ $(LD_FLAGS)\n\n$(OBJ_DIR)/%.o: src/%.c\n\tmkdir -p $(@D)\n\t$(CC) $(CFLAGS) -c $< -o $@\n\nclean:\n\trm -rf $(OBJ_DIR) $(BIN_DIR)", wildcard, objs, proj_name, proj_name)
-} else {
-    format!("SRCS := {}\nOBJS := {}\n\nall: directories {}\n\ndirectories:\n\tmkdir -p $(OBJ_DIR) $(BIN_DIR)\n\n{}: $(OBJS)\n\t$(CC) $(CFLAGS) -o $(BIN_DIR)/$@ $^ $(LD_FLAGS)\n\n$(OBJ_DIR)/%.o: %.c\n\tmkdir -p $(@D)\n\t$(CC) $(CFLAGS) -c $< -o $@\n\nclean:\n\trm -rf $(OBJ_DIR) $(BIN_DIR)", wildcard, objs, proj_name, proj_name)
-};
+fn generate_make_file(
+    use_src_dir: bool,
+    wildcard: String,
+    objs: String,
+    proj_name: String,
+) -> String {
+    if use_src_dir {
+        format!(
+            "SRCS := {}\n\
+            OBJS := {}\n\
+            \n\
+            all: directories {}\n\
+            \n\
+            directories:\n\
+            \tmkdir -p $(OBJ_DIR) $(BIN_DIR)\n\
+            \n\
+            {}: $(OBJS)\n\
+            \t$(CC) $(CFLAGS) -o $(BIN_DIR)/$@ $^ $(LD_FLAGS)\n\
+            \n\
+            $(OBJ_DIR)/%.o: src/%.c\n\
+            \tmkdir -p $(@D)\n\
+            \t$(CC) $(CFLAGS) -c $< -o $@\n\
+            \n\
+            clean:\n\
+            \trm -rf $(OBJ_DIR) $(BIN_DIR)\n\
+            \n\
+            run: {}\n\
+            \t./$(BIN_DIR)/{}",
+            wildcard, objs, proj_name, proj_name, proj_name, proj_name
+        )
+    } else {
+        format!(
+            "SRCS := {}\n\
+            OBJS := {}\n\
+            \n\
+            all: directories {}\n\
+            \n\
+            directories:\n\
+            \tmkdir -p $(OBJ_DIR) $(BIN_DIR)\n\
+            \n\
+            {}: $(OBJS)\n\
+            \t$(CC) $(CFLAGS) -o $(BIN_DIR)/$@ $^ $(LD_FLAGS)\n\
+            \n\
+            $(OBJ_DIR)/%.o: %.c\n\
+            \tmkdir -p $(@D)\n\
+            \t$(CC) $(CFLAGS) -c $< -o $@\n\
+            \n\
+            clean:\n\
+            \trm -rf $(OBJ_DIR) $(BIN_DIR)\n\
+            \n\
+            run: {}\n\
+            \t./$(BIN_DIR)/{}",
+            wildcard, objs, proj_name, proj_name, proj_name, proj_name
+        )
+    }
+}
 
-let makefile_content = format!("{}\n\n{}", make_vars, make_target_all);
+fn create_main_c(path: &str) {
+    let c_boiler_plate = "#include <stdio.h>\nint main(int argc, char *argv[]) {\n\treturn 0;\n}";
+    std::fs::write(path, c_boiler_plate).unwrap();
+}
 
-return makefile_content;}
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
-    let _pwd = &args[0];
-
     let stdin = std::io::stdin();
 
     let proj_name = read_option(&stdin, "Enter a project name");
-    let use_src_dir = read_option(&stdin, "Use a 'src' dir? [y/n]");
-    let use_inc_dir = read_option(&stdin, "Use a 'include' dir? [y/n]");
+    let use_src = read_option(&stdin, "Use a 'src' dir? [y/n]").to_lowercase() == "y";
+    let use_include = read_option(&stdin, "Use a 'include' dir? [y/n]").to_lowercase() == "y";
     let libraries = read_several_options(&stdin, "Enter any libraries you want to link against (omit the -l) and type 'done' when you're finished.");
 
-    let content = create_makefile(proj_name, use_src_dir == "y", use_inc_dir == "y", libraries);
+    // generate the makefile.
+    let content = generate_makefile(proj_name, use_src, use_include, libraries);
 
-    println!("{}", content);
-
+    // write out files.
     std::fs::write("Makefile", content).unwrap();
-    let c_boiler_plate =  "#include <stdio.h>\nint main(int argc, char *argv[]) {\nreturn 0;\n}";
-    if use_src_dir.to_lowercase() == "y" {
-        std::fs::create_dir("src").unwrap();
-        std::fs::write("src/main.c", c_boiler_plate).unwrap();
-    } else {
-        std::fs::write("main.c", c_boiler_plate).unwrap();
-    }
-    if use_inc_dir.to_lowercase() == "y" {
+    if use_include {
         std::fs::create_dir("include").unwrap();
+    }
+    
+    if use_src {
+        std::fs::create_dir("src").unwrap();
+        create_main_c("src/main.c");
+    } else {
+        create_main_c("main.c");
     }
 }
